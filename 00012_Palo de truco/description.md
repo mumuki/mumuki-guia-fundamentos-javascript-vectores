896488bae158c14953d309c3a8902405b679118e
Franco Leonardo Bulgarelli <flbulgarelli@yahoo.com.ar>
2016-05-15 11:19:40 -0300

    Agregando ejercicio 12

---
+++ 00012_Palo de truco/description.md
@@ -0,0 +1,10 @@
+¡Hora de practicar! 
+
+Queremos escribir una función `paloDeTruco`, que dado un palo, devuelva una lista de strings, una por cada carta de ese palo según las cartas del truco: 
+
+```javascript
+ム paloDeTruco("espadas")
+["1 de espadas", "2 de espadas", "3 de espadas" ..., "12 de espadas"]
+```
+
+Tomá en cuenta que las cartas de truco incluyen todas las numeradas del 1 al 12, excepto por los 8s y los 9s. 
---
+++ 00012_Palo de truco/test.js
@@ -0,0 +1,37 @@
+describe("paloDeTruco", function() {
+  var paloAlAzar =  Math.random().toString(36).substring(2);
+  
+  it('paloDeTruco("espadas")', function() {
+    assert.equal(
+      paloDeTruco("espadas"), 
+      [ '1 de espadas',
+        '2 de espadas',
+        '3 de espadas',
+        '4 de espadas',
+        '5 de espadas',
+        '6 de espadas',
+        '7 de espadas',
+        '10 de espadas',
+        '11 de espadas',
+        '12 de espadas' ]);
+  });
+  
+   it('paloDeTruco("bastos")', function() {
+    assert.equal(
+      paloDeTruco("espadas"), 
+      [ '1 de bastos',
+        '2 de bastos',
+        '3 de bastos',
+        '4 de bastos',
+        '5 de bastos',
+        '6 de bastos',
+        '7 de bastos',
+        '10 de bastos',
+        '11 de bastos',
+        '12 de bastos' ]);
+        
+        
+   it('paloDeTruco("'+paloAlAzar+'")', function() {
+    assert(paloDeTruco(paloAlAzar).indexOf(paloAlAzar) > 0);
+  });
+})
