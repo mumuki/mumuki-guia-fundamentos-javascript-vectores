7f6b7762cacfc7e5cc536045b6aee733758ad1c8
franco@mumuki.org <franco@mumuki.org>
2016-05-19 12:19:49 -0400

    Mumuki Export on 2016-05-19 12:19:49 -0400

---
+++ 00011_Y esto, es un array/extra.js
@@ -0,0 +1,13 @@
+function arraysIguales(unArray, otroArray) {
+    if (unArray.length != otroArray.length)
+        return false;
+    for (var i = 0, i < unArray.length; i++) {
+        if (unArray[i] instanceof Array && otroArray[i] instanceof Array) {
+            if (!arraysIguales(unArray[i], otroArray[i]))
+              return false;
+        }           
+        else if (unArray[i] != array[i])
+          return false;   
+    }       
+    return true;
+}
