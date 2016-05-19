660dcd02449d88a82b09ab1c2c166842894928c1
franco@mumuki.org <franco@mumuki.org>
2016-05-19 12:04:28 -0400

    Mumuki Export on 2016-05-19 12:04:28 -0400

--- 00012_Palo de truco/test.js
+++ 00012_Palo de truco/test.js
@@ -1,21 +1,13 @@
 describe("paloDeTruco", function() {
   var paloAlAzar =  Math.random().toString(36).substring(2);
 
-  it('paloDeTruco("espadas") NO debería contenerel "8 de espadas"', function() {
-    assert.deepEqual(
-      paloDeTruco("espadas"),
-      [ '1 de espadas',
-      '2 de espadas',
-      '3 de espadas',
-      '4 de espadas',
-      '5 de espadas',
-      '6 de espadas',
-      '7 de espadas',
-      '10 de espadas',
-      '11 de espadas',
-      '12 de espadas' ]);
+  it('paloDeTruco("espadas") NO debería contener el "8 de espadas"', function() {
+    assert(paloDeTruco("espadas").indexOf("8 de espadas") < 0));
   });
 
+  it('paloDeTruco("espadas") NO debería contenerel "9 de espadas"', function() {
+    assert(paloDeTruco("espadas").indexOf("9 de espadas") < 0));
+  });
 
   it('paloDeTruco("espadas") debería ser ["1 de espadas", "2 de espadas"...', function() {
     assert.deepEqual(
